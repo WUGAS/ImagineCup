831e36be10639dde8f872c32676ae4ff8ab5dab0
Suhong Jin <suhongjin94@gmail.com>
2014-12-12 23:57:22 -0600

    first android app commit

---
+++ ImagineCup/src/com/wugas/imaginecup/MainScreen.java
@@ -0,0 +1,11 @@
+package com.wugas.imaginecup;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainScreen extends Activity{
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_screen);
+	}
+}
